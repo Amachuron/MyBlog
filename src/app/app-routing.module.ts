c77fd0a712726ea3e495c22ff16b02ebf4cb30fe
Alexandre Machuron <a.machuron@gmail.com>
2018-12-04 00:02:53 +0100

    'bla'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {SectionComponent} from "./home/section/section.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'Accueil', component: SectionComponent },
+  { path: '', redirectTo: '/Accueil', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
